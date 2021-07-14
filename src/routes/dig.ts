e895062abb47b4aadcac9e0ab80292d8dbd0a0a4
Eldar Polishchuk <eldar.polishchuk@gmail.com>
2021-07-15 01:20:44 +0300

    DIG: add custom NS server support

--- src/routes/dig.ts
+++ src/routes/dig.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import net from 'net';
 import { dig } from '../dns-utils';
 import { digAnswer } from '../interfaces';
 
@@ -43,6 +44,54 @@ router.get('/dig/:host/:record', async (req, res) => {
   }
   try {
     answer.nameservers = await dig(answer.host, NS);
+    return res.json({ answer });
+  } catch (error) {
+    answer.nameservers = error;
+    return res.json({ answer });
+  }
+});
+
+router.get('/dig/:host/:record/:server', async (req, res) => {
+  let answer: digAnswer = {
+    host: req.params.host,
+  };
+
+  let record: string = req.params.record;
+  let server: string = req.params.server;
+
+  if (net.isIP(server) === 0) {
+    let serverIp;
+    try {
+      serverIp = await dig(
+        server,
+        'A');
+    } catch (error) {
+      if (error === 'ENOTFOUND') {
+        answer.error = error;
+      }
+      return res.json({ answer });
+    }
+    if (Array.isArray(serverIp)) {
+      const random = Math.floor(Math.random() * serverIp.length);
+      server = String(serverIp[random]);
+    }
+  }
+  try {
+    answer.record = await dig(
+      answer.host,
+      record,
+      server
+    );
+  } catch (error) {
+    if (error === 'ENOTFOUND') {
+      answer.error = error;
+      return res.json({ answer });
+    }
+    answer.record = error;
+  }
+
+  try {
+    answer.nameservers = await dig(answer.host, NS, server);
     return res.json({ answer });
   } catch (error) {
     answer.nameservers = error;
